db5cfdd3bd52b0d1c0079df673c98e15e91ab46d
Ivan Ushmorov <ivan.ushmorov@raysono.com>
2018-01-02 15:32:06 +0100

    finished the function to save the json file to s3 bucket

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # gpiocontroll
-Alexa Skill to controll Raspberry Pi GPIOs
+Alexa Skill to control Raspberry Pi GPIOs
+
+## Install:
+`npm install`
+
+## Lambda function:
+Zip the following files: index.js, package.json and node_modules
+
+## Alexa Skill
+Everything you need in the **alexa_skill** folder.
--- index.js
+++ index.js
@@ -1,40 +1,49 @@
 'use strict';
 var Alexa = require("alexa-sdk");
 var appId = 'amzn1.ask.skill.caa93b39-97a4-4746-9451-1f6b2427c016'; //'amzn1.echo-sdk-ams.app.your-skill-id';
-var s3saver = require('./s3saver');
+var aws = require('aws-sdk');
+var s3 = new aws.S3();
+
+var newSessionHandlers = {
+
+    //neccessery fallback function do not delete
+    'Unhandled': function () {
+        this.emit(':ask', 'ERROR Unhandled Session');
+    },
+    'GPIOControlIntent': function () {
+
+        var obj = {
+            table: []
+        };
+
+        var pin = this.event.request.intent.slots.pin.value.toLowerCase(),
+            status = this.event.request.intent.slots.status.value.toLowerCase();
+        obj.table.push({pin: pin, state: status});
+
+        var json = JSON.stringify(obj);
+
+        let params = {
+            Bucket: 'gpiocontroll', // your bucket name,
+            Key: 'test.txt', // path to the object you're looking for
+            Body: json
+        };
+
+        var putObjectPromise = s3.putObject(params).promise();
+
+        putObjectPromise.then((data) => {
+            console.log('Success');
+            this.emit(':tell', 'Ich habe den Pin ' + pin + ' auf ' + status + ' gesetzt.');
+        }, (err) => {
+            console.log(err);
+            this.emit(':tell', 'Es ist ein Fehler aufgetreten');
+        });
+
+    }
+};
 
 exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.appId = appId;
     alexa.registerHandlers(newSessionHandlers); //add more handlers here, z.B.: alexa.registerHandlers(newSessionHandlers, guessModeHandlers, startGameHandlers, guessAttemptHandlers);
     alexa.execute();
-};
-
-function saveTo(data){
-    console.log('hello');
-    var json = JSON.stringify(data);
-    s3saver.saveJSONtoS3(json);
-}
-
-var newSessionHandlers = {
-    //neccessery fallback function do not delete
-    'Unhandled': function() {
-    this.emit(':ask', 'ERROR Unhandled Session');
-    },
-    'GPIOControlIntent': function () {
-
-        var obj = {
-               table: []
-            };
-
-        var pin = this.event.request.intent.slots.pin.value.toLowerCase(),
-            status = this.event.request.intent.slots.status.value.toLowerCase();
-            obj.table.push({pin: pin, state: status});
-
-        console.log('OBJ', obj);
-
-        saveTo(obj);
-
-        this.emit(':tell', 'Ich habe den Pin ' + pin + ' auf ' + status + ' gesetzt.');
-    }
 };
